7cdffd2c421c063b275ec6ce56f13debbb6fc1e0
Marco Prado <marco.prado@gsw.com.br>
2020-12-09 22:39:55 -0300

    Initial commit

---
+++ src/main/java/com/marco/nexusapp/NexusappApplication.java
@@ -0,0 +1,13 @@
+package com.marco.nexusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marco/nexusapp/NexusappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.nexusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
